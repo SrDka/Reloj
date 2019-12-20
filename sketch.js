90efc1a669d3e8fe88a9cc05cb630a0b2b72ea24
SrDka <59050148+SrDka@users.noreply.github.com>
2019-12-20 09:45:40 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(1300, 400);
+  frameRate(60);
+}
+
+function draw() {
+  background(220);
+  noStroke();
+  ellipse(mouseX,mouseY,50);
+  fill(255,0,0);
+  
+  
+}
